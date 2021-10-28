6d6fcf51f79e45f38ff12f7dc6a448d4cd169c1e
seven7thmoon <porklong@gmail.com>
2021-10-28 20:54:18 +0800

    L7 practice by my own method

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const app = Vue.createApp({
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+            ],
         }
     },
     methods: {
