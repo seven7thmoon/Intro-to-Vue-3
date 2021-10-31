2871e7f268950301a173160509bc786392e3981c
seven7thmoon <porklong@gmail.com>
2021-10-31 23:13:07 +0800

    before practice by my own method

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -41,7 +41,8 @@ app.component('product-display', {
 
       </div>
     </div>
-    <review-form></review-form>
+    <review-list v-if="reviews-length" :reviews="reviews"></review-list>
+    <review-form @review-submitted="addReview"></review-form>
   </div>`,
   data() {
     return {
@@ -52,7 +53,8 @@ app.component('product-display', {
         variants: [
           { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
           { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-        ]
+        ],
+        reviews: []
     }
   },
   methods: {
@@ -61,6 +63,9 @@ app.component('product-display', {
       },
       updateVariant(index) {
           this.selectedVariant = index
+      },
+      addReview(review) {
+          this.reviews.push(review)
       }
   },
   computed: {
